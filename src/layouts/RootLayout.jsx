40633022e827d2e9469e99d4ea942eeb54f797fc
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-09-04 08:56:49 +0100

    RootLayout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,16 +1,23 @@
-import React from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { NavLink, Outlet, useLocation, useParams } from "react-router-dom";
 import { FaBars } from "react-icons/fa6";
 import { SlNotebook } from "react-icons/sl";
+import { PathContext } from "../helpers/pathContext";
 import "./RootLayout.css";
 
 function RootLayout() {
+	const { path } = useContext(PathContext);
+
 	return (
 		<div className="root-layout">
-			<h1 className="root-h1">All Notes</h1>
+			<h1 className="root-h1">{path}</h1>
 			<nav className="nav">
 				{/* <SlNotebook size={"2rem"} /> */}
 				<ul className="nav-ul">
+					<li className="nav-list">
+						{" "}
+						<button>All Notes</button>
+					</li>
 					<li className="nav-list">
 						{" "}
 						<NavLink to="/">All Notes</NavLink>
