1b6d3c256b0f96bc8eb84dd95850b5a55fe58e25
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-09-04 09:23:05 +0100

    Update RootLayout.jsx

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,6 +2,9 @@ import React, { useContext, useState } from "react";
 import { NavLink, Outlet, useLocation, useParams } from "react-router-dom";
 import { FaBars } from "react-icons/fa6";
 import { SlNotebook } from "react-icons/sl";
+import { AiOutlinePlus } from "react-icons/ai";
+import { BsPlus } from "react-icons/bs";
+import { BsPlusLg } from "react-icons/bs";
 import { PathContext } from "../contexts/pathContext";
 import "./RootLayout.css";
 
@@ -14,16 +17,30 @@ function RootLayout() {
 			<nav className="nav">
 				{/* <SlNotebook size={"2rem"} /> */}
 				<ul className="nav-ul">
-					<li className="nav-list">
-						{" "}
-						<button>All Notes</button>
+					<li className="nav-list nav-button">
+						<button>
+							<svg
+								stroke="currentColor"
+								fill="none"
+								strokeWidth="2"
+								viewBox="0 0 24 24"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								class="h-4 w-4 shrink-0"
+								height="1em"
+								width="1em"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<line x1="12" y1="5" x2="12" y2="19"></line>
+								<line x1="5" y1="12" x2="19" y2="12"></line>
+							</svg>{" "}
+							All Notes
+						</button>
 					</li>
 					<li className="nav-list">
-						{" "}
 						<NavLink to="/">All Notes</NavLink>
 					</li>
 					<li className="nav-list">
-						{" "}
 						<NavLink to="/favorite">Favorites</NavLink>
 					</li>
 					<li className="nav-list">
