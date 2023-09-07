e38032271efd882760a4515689b9ceb22a50813f
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-09-07 07:34:11 +0100

    NewNote Update

--- src/pages/NewNote.jsx
+++ src/pages/NewNote.jsx
@@ -1,17 +1,25 @@
 import React, { useContext, useEffect } from "react";
 import { FaChevronLeft } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import { MdDelete, MdFavoriteBorder, MdOutlineDelete } from "react-icons/md";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
 import { NoteContext } from "../contexts/NoteContext";
 import "./NewNote.css";
+import { FiStar } from "react-icons/fi";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 
 function NewNote() {
 	const { setPath } = useContext(NoteContext);
 	const navigate = useNavigate();
 	const handleBackButton = (e) => {
 		e.preventDefault();
-		navigate(-1);
+		navigate("/");
 
 		console.log("back clicked");
+	};
+
+	const handleButton = (e) => {
+		e.preventDefault();
 	};
 	useEffect(() => {
 		setPath("New Note");
@@ -24,6 +32,17 @@ function NewNote() {
 						<FaChevronLeft color="white" size="1.5rem" />
 					</button>
 					<input className="title-input" placeholder="Title" />
+					<button onClick={handleButton} className="favorite-button">
+						<FiStar title="Add to favorite" size="1.5rem" className="favorite-svg" />
+					</button>
+					<button onClick={handleButton} className="delete-button">
+						<MdOutlineDelete
+							size="1.6rem"
+							color="white"
+							className="delete-svg"
+							title="Delete Note"
+						/>
+					</button>
 				</div>
 				<textarea placeholder="Note..." />
 			</form>
