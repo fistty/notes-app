99f588b5e271691110ce0250f12bbfa754b5d29c
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-09-29 23:28:15 +0100

    error

--- src/pages/note/noteLoader.js
+++ src/pages/note/noteLoader.js
@@ -2,7 +2,7 @@ export const noteLoader = ({ params }, notes) => {
 	const { id } = params;
 	const noteDetail = notes.find((noteItem) => noteItem.id === id);
 	if (!noteDetail) {
-		console.log(404);
+		console.log(404, "from NOTELOADER");
 		return { title: "...", note: "..." };
 	}
 	return noteDetail;
--- src/pages/trash/trashNoteLoader.js
+++ src/pages/trash/trashNoteLoader.js
@@ -2,7 +2,7 @@ export const trashNoteLoader = ({ params }, deletedNotes) => {
 	const { id } = params;
 	const deletedNoteDetail = deletedNotes.find((noteItem) => noteItem.id === id);
 	if (!deletedNoteDetail) {
-		console.log(404);
+		console.log(404, "from TRASH NOTE LOADER");
 		return { title: "...", note: "..." };
 	}
 	return deletedNoteDetail;
