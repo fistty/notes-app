d21f88a0a03a53caef50c0d9f5ebf15c341a1131
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-09-29 03:09:23 +0100

    Update noteLoader.js

--- src/pages/note/noteLoader.js
+++ src/pages/note/noteLoader.js
@@ -1,6 +1,6 @@
 export const noteLoader = ({ params }, notes) => {
 	const { id } = params;
-	const noteDetail = notes.find((noteItem) => noteItem.id == id);
+	const noteDetail = notes.find((noteItem) => noteItem.id === id);
 	if (!noteDetail) {
 		console.log(404);
 		return { title: "...", note: "..." };
