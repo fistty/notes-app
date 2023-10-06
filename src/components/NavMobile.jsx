1fcc919654227f5dbe968f378386cf1338df02a7
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-10-06 13:18:47 +0100

    mobile nav display logic

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -8,11 +8,19 @@ export const NavMobile = () => {
 	const { setIsModal } = useNoteContext();
 
 	const handleClick = () => {
-		const nav = document.querySelector(".nav-ul");
-		nav.classList.add("active");
+		// To get the side menu to stay on top of the screen
+		const navUl = document.querySelector(".nav-ul");
+		const nav = document.querySelector(".nav");
+
+		// Calculates the distance of the nav element from the top of the page
+		// and the distance in which the window has been scrolled
+		let offset = nav.offsetTop - window.scrollY;
+		navUl.style.top = `-${offset}px`;
+		navUl.classList.add("active");
 		setIsModal(true);
 		bodyScrollToggle.disable();
 		// Allows for the hamburger menu to work when not on top
+
 		const body = document.querySelector("body");
 		body.style.height = "";
 		body.style.overflow = "";
