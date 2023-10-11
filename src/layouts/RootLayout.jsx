c7e319b1fb9c8746a4b1792a8a8fb7d497f6b089
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-10-11 07:31:17 +0100

    Update RootLayout.jsx

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,14 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../components/Nav";
-import { useNoteContext } from "../contexts/noteContext/useNoteContext";
 import "./RootLayout.css";
 
 function RootLayout() {
-	const { path } = useNoteContext();
-
 	return (
 		<div className="root-layout">
-			{/* <h1 className="root-h1">{path}</h1> */}
 			<Nav />
 			<main>
 				<Outlet />
