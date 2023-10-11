b194dfac6f00bbabb7453625ff72af3fe9a43874
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-10-11 07:31:07 +0100

    modal and backdorp improvement

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -2,20 +2,13 @@ import { FaBars } from "react-icons/fa6";
 import { NewNoteButton } from "./NewNoteButton";
 import { useNoteContext } from "../contexts/noteContext/useNoteContext";
 import { disableBodyScroll } from "../utils/helpers/bodyScroll";
-import { useEffect } from "react";
-
+import { Backdrop } from "./Backdrop";
 export const NavMobile = () => {
-	const { setIsBackdrop } = useNoteContext();
+	const { isBackdrop, isModal, setIsBackdrop } = useNoteContext();
 
 	const handleClick = () => {
 		// To get the side menu to stay on top of the screen
 		const navUl = document.querySelector(".nav-ul");
-		const nav = document.querySelector(".nav");
-
-		// // Calculates the distance of the nav element from the top of the page
-		// // and the distance in which the window has been scrolled
-		// let offset = nav.offsetTop - window.scrollY;
-		// navUl.style.top = `-${offset}px`;
 		navUl.classList.add("active");
 		navUl.classList.add("act");
 		setIsBackdrop(true);
@@ -35,6 +28,7 @@ export const NavMobile = () => {
 				</button>
 			</li>
 			<li className="nav-mobile-li nav-mobile-new">
+				{(isModal || isBackdrop) && <Backdrop />}
 				<NewNoteButton className="nav-list-new" />
 			</li>
 		</ul>
