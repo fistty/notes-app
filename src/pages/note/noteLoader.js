ed70737687ce76460f35ac0bb91aa951e8fc3db7
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-10-14 14:13:41 +0100

    Update noteLoader.js

--- src/pages/note/noteLoader.js
+++ src/pages/note/noteLoader.js
@@ -2,8 +2,7 @@ export const noteLoader = async ({ params }, notes) => {
 	const { id } = params;
 	const noteDetail = await notes.find((noteItem) => noteItem.id === id);
 	if (!noteDetail) {
-		console.log(404, "from NOTELOADER");
-		return { title: "...", note: "..." };
+		throw new Error("Could not find such note");
 	}
 	return noteDetail;
 };
